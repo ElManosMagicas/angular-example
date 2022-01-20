a9ff012179d197ee4ea082b0cbe23a9adb7de385
ElManosMagicas <ing.juandgarayc@gmail.com>
2022-01-20 16:54:24 -0500

    first html structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginBoxComponent } from './components/login-box/login-box.component';
+import { TitleComponent } from './components/title/title.component';
+import { FormComponent } from './components/form/form.component';
+import { SocialMediaComponent } from './components/social-media/social-media.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginBoxComponent,
+    TitleComponent,
+    FormComponent,
+    SocialMediaComponent
   ],
   imports: [
     BrowserModule,
